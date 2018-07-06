bfdb38286bc3486285a47afcd561869aedf4d79b
Pratikag123 <pratik.agrawal@bresotec.com>
2018-07-05 21:32:16 -0400

    Unity files added

---
+++ Assets/MoveRacket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour {
+    public float speed = 30;
+    public string axes = "Vertical";
+
+    void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw(axes);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v)*speed;
+    }
+}
